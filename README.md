71dca65c9f46fb6b31aa9da44213f83676cc2827
Nathan <nlaundry@uoguelph.ca>
2021-03-11 14:50:49 -0500

    Initial hello world application with babelrc for easier imports

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Cinee
+
+## Running the project
+
+npm start
+Scan QR code using the expo app on a mobile device
---
+++ src/components/atoms/hello-world.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+type Greeting = {
+    name: string
+}
+
+const HelloWorld = ({ name }: Greeting) => <Text> Hello World {name}! </Text>
+
+export default HelloWorld
