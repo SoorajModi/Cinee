85ea1e60bd029135fd432fec1baba919ffa9d336
Nathan <nlaundry@uoguelph.ca>
2021-03-17 13:46:52 -0400

    initial firebase setup with example read and write to realtime database in homescene

--- App.jsx
+++ App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import Navigator from '@navigations'
+import firebase from 'firebase';
+import firebaseConfig from './firebaseConfig'
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
--- src/scenes/home/HomeScene.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { HelloWorld } from '@atoms'
-
-const HomeScene = () => <HelloWorld name="This is the home screen" />
-
-export default HomeScene;
