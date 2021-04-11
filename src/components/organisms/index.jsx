a05ef55dbdd25a89881e7d7d66b530eceb4a302c
Nathan <nlaundry@uoguelph.ca>
2021-04-11 15:13:14 -0400

    added MatchesLIst to exports

--- src/components/organisms/index.jsx
+++ src/components/organisms/index.jsx
@@ -1,9 +1,11 @@
 import LoginForm from './LoginForm';
 import BrowseList from './BrowseList';
 import LikedList from './LikedList';
+import MatchesList from './MatchesList';
 
 export {
   LoginForm,
   BrowseList,
   LikedList,
+  MatchesList
 };
